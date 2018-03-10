d8c73e5a069f4a7b148673574f44703ad1b9aade
szaman <shamsadzaman@yahoo.com>
2018-03-10 19:03:09 +0600

    show unique words to console, reading from a txt file

---
+++ ReadFile/FileHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReadFile
+{
+    public class FileHandler
+    {
+        private readonly string[] _fileDirectories;
+
+        public Dictionary<string, int> WordCountDictionary { get; private set; }
+
+        public FileHandler(string[] fileDirectories)
+        {
+            _fileDirectories = fileDirectories;
+            if (WordCountDictionary == null)
+            {
+                WordCountDictionary = new Dictionary<string, int>();
+            }
+        }
+
+        public void WriteDictionaryData()
+        {
+            foreach (var word in WordCountDictionary)
+            {
+                Console.WriteLine($"{word.Key}\t\t: {word.Value}");
+            }
+        }
+
+        public void Start()
+        {
+            foreach (var fileDirectory in _fileDirectories)
+            {
+                if (!Directory.Exists(fileDirectory))
+                {
+                    Console.WriteLine("ERROR: File doesn't exist");
+                    continue;
+                }
+
+                var dir = new DirectoryInfo(fileDirectory);
+
+                var files = dir.GetFiles("*.txt");
+
+                foreach (var file in files)
+                {
+                    using (var streamReader = File.OpenText(file.FullName))
+                    {
+                        Console.Write($"Processing: {file.FullName}.....");
+                        string line;
+                        while ((line = streamReader.ReadLine()) != null)
+                        {
+                            var words = line.Split(' ', ',', '.');
+
+                            foreach (var word in words)
+                            {
+                                if (WordCountDictionary.ContainsKey(word))
+                                {
+                                    WordCountDictionary[word]++;
+                                }
+                                else
+                                {
+                                    WordCountDictionary.Add(word, 1);
+                                }
+                            }
+                        }
+                        Console.WriteLine("..Done");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ReadFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] directories)
+        {
+            string[] fileDirectories = null;
+            if (directories != null && directories.Any())
+            {
+                fileDirectories = directories;
+            }
+            else
+            {
+                fileDirectories = Console.ReadLine()?.Split(' ');
+            }
+
+            var fileHandler = new FileHandler(fileDirectories);
+            fileHandler.Start();
+            fileHandler.WriteDictionaryData();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
